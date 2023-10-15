93ae0f2b50e2e682d719e8ead60ea856cc58ac5e
Michael <88445485+mjnyland@users.noreply.github.com>
2023-10-14 20:20:41 -0400

    Project logic and styling added

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,10 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctCountry = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    
     var body: some View {
-        VStack {
-            Text("Wat")
+        
+        ZStack {//A Z Stack to be contain all and be a background
+            
+            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack {//A stack to contain the two below
+                
+                VStack { //Text Content
+                    Text("Select the flag for")
+                        .foregroundStyle(.white)
+                        .font(.subheadline.weight(.semibold))
+                    
+                    Text(countries[correctCountry])
+                        .foregroundStyle(.white)
+                        .font(.largeTitle.weight(.semibold))
+                }
+                
+                VStack (spacing: 16){ //A stack for the flags
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            checkAnswer(number)
+                        } label: {
+                            Image(countries[number])
+                                .shadow(radius: 5)
+                        }
+                        
+                    }
+                }
+            }
+            
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
+        }
+    }
+    
+    func checkAnswer(_ answer: Int) {
+        if (correctCountry == answer) {
+            scoreTitle = "You're correct!"
+        } else {
+            scoreTitle = "You're incorrect!"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion(){
+        countries.shuffle()
+        correctCountry = Int.random(in: 0...2)
     }
 }
 
